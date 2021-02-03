36393528de3cf99916b2eac5dca44f98f59aee80
Violeta Hernández <eric.ivan.hdz@gmail.com>
2021-02-03 15:23:28 -0600

    Update swirl.js
    Added cubeswirls.

--- src/extras/swirl.js
+++ src/extras/swirl.js
@@ -26,10 +26,31 @@ export const tetswirl = function(n) {
 }
 globalThis.tetswirl = tetswirl;
 
-/*export const cubeswirl = function(n) {
+export const cubeswirl = function(n) {
     const coords = [];
+    const k1 = Math.sqrt(3 + Math.sqrt(3)),
+        k2 = Math.sqrt(3 - Math.sqrt(3));
 
     for(let k = 0; k < 2 * n; k++) {
-        
+        const cos1 = math.cos(k, 4 * n),
+            sin1 = math.sin(k, 4 * n),
+            cos2 = math.cos(2 * k + n, 8 * n),
+            sin2 = math.sin(2 * k + n, 8 * n);
+
+        for(let i = 0; i < 4; i++) {
+            const cos1_ = math.cos(k + i * n, 4 * n),
+                sin1_ = math.sin(k + i * n, 4 * n),
+                cos2_ = math.cos(2 * k + (2 * i + 1) * n, 8 * n),
+                sin2_ = math.sin(2 * k + (2 * i + 1) * n, 8 * n);
+
+            const c1 = [cos1_ / k1, sin1_ / k1, cos1 / k2, sin1 / k2];
+            coords.push(c1, c1.map(neg));
+
+            const c2 = [cos2_ / k2, sin2_ / k2, cos2 / k1, sin2 / k1];
+            coords.push(c2, c2.map(neg));
+        }
     }
-}*/
+
+    return coords;
+}
+globalThis.cubeswirl = cubeswirl;
